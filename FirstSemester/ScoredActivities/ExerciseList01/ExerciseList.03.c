933db2432a3e5e1aba2f1ea81d2b6fe0822f84e9
Bruno Correia <70177658@amcontratos.com.br>
2026-03-19 19:38:23 -0300

    [FEAT]: Exercise03 from ExerciseList01 is done

---
+++ FirstSemester/ScoredActivities/ExerciseList01/ExerciseList.03.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+    //vou usar bitwise com XOR para inverter, pura gracinha minha, 
+    //mas em sistemas embarcados isso é mais rapido do que usar uma variavel auxiliar, então ta valendo
+    
+    int numA;
+    int numB;
+
+    printf("Digite o primeiro numero: ");
+    if (scanf("%d", &numA) != 1)
+        return 1;
+
+    printf("Digite o segundo numero: ");
+    if (scanf("%d", &numB) != 1)
+        return 1;
+
+    numA = numA ^ numB;
+    numB = numA ^ numB;
+    numA = numA ^ numB;
+
+    printf("\nPrimeiro numero depois da inversão: %d\n", numA);
+    printf("Segundo numero depois da inversão: %d\n", numB);
+
+    return 0;
+}
